5d5e59add25baa2bb7f8f1b73ae0d8878236fc34
Justus Bringer <rtuz2th@justuss-mbp.speedport.ip>
2025-06-25 20:12:17 +0200

    Updated Parser

--- api/events.js
+++ api/events.js
@@ -50,7 +50,6 @@ export default async function handler(req, res) {
         };
       });
 
-
     res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
     res.status(200).json(events);
   } catch (error) {
